952038dc9ff22be563e4256d621c2db1a0885860
mahesh <pma2wrn@bosch.com>
2022-12-23 17:12:58 +0530

    my first app

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='container'>
+      <div className='row'>
+        <div className='col-12'>
+          <p className='lead'>App</p>
+          <button type='button' className='btn btn-primary'>click me</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
